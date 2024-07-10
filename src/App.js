c6cadb37eac5bc01cbf4603dd2328faf60e2bd05
Elise Frazier <efrazie7@uncc.edu>
2024-07-09 23:27:40 -0400

    add footer and footer logo

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero/>
-
+      <Footer/>
     </>
   );
 }
