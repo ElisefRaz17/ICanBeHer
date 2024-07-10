8848f8a8b312ea662f1df3d7b09ffcce51481ff3
Elise Frazier <efrazie7@uncc.edu>
2024-07-09 23:11:53 -0400

    add select dropdown on homepage

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,47 @@
-import React from 'react'
+import React from "react";
 import styles from "../styling/hero.module.css";
 import HeroImage from "../assets/HeroImage.png";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 function Hero() {
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
   return (
-    <div className={styles.background}>
-        <div className={styles.content}>
-            <img src={HeroImage} alt="hero image"/>
-            <h4>What role in tech do you want to pursue or currently have?</h4>
+    <>
+      <div className={styles.content}>
+        <img src={HeroImage} alt="hero image" style={{marginLeft:'auto'}} />
+        {/* <Box sx={{ minWidth: 120 }}> */}
+        <div className="content-contaienr">
+          <div style={{ textAlign: "center", color: "black", fontFamily:'Roboto' }}>
+            <p>What role in tech do you want to pursue or currently have?</p>
+          </div>
+          <div style={{ textAlign: "center" }}>
+            <FormControl sx={{ m: 1, minWidth: 200 }}>
+              <InputLabel id="demo-simple-select-label">Role</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={age}
+                label="Algorithm"
+                onChange={handleChange}
+              >
+                <MenuItem value={1}>Developer</MenuItem>
+                <MenuItem value={2}>Entrepeneur</MenuItem>
+                <MenuItem value={3}>Engineer</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
         </div>
-    </div>
-  )
+      </div>
+      <div className={styles.background}></div>
+    </>
+  );
 }
 
-export default Hero
+export default Hero;
