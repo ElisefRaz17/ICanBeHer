c1f22646f6dd78fa2009707aa69fe54ec3bc7c1d
Elise Frazier <efrazie7@uncc.edu>
2024-07-09 22:37:29 -0400

    centered hero image and text on home page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import styles from "../styling/hero.module.css";
-import HeroImage from "../assets/HeroBackground.png";
+import HeroImage from "../assets/HeroImage.png";
 function Hero() {
   return (
-    <div className={styles.background} style={{backgroundImage: `url(${HeroImage})`}}>
+    <div className={styles.background}>
+        <div className={styles.content}>
+            <img src={HeroImage} alt="hero image"/>
+            <h4>What role in tech do you want to pursue or currently have?</h4>
+        </div>
     </div>
   )
 }
