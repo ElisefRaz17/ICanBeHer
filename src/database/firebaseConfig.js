16439313559b27e092d6a3d7bdb14f26be10c55d
Elise Frazier <efrazie7@uncc.edu>
2024-07-13 15:41:37 -0400

    add login and signup placeholders and firebase configuration

---
+++ src/database/firebaseConfig.js
@@ -0,0 +1,10 @@
+let firebaseConfig = {
+    apiKey: "AIzaSyDtsgHspBq5cP_PY93E-siKavg8PKE9IeA",
+    authDomain: "icanbeher-52758.firebaseapp.com",
+    projectId: "icanbeher-52758",
+    storageBucket: "icanbeher-52758.appspot.com",
+    messagingSenderId: "219706161343",
+    appId: "1:219706161343:web:6f266353dc1cfb9bc12e0a"
+  };
+
+export default firebaseConfig;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {firebase} from "@firebase/app";
+import firebaseConfig from './database/firebaseConfig';
 
+firebase.initializeApp(firebaseConfig);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
