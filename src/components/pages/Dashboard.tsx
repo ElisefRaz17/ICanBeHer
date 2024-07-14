d4ab19bf492b592279e34daa0976abac3564cb72
Elise Frazier <efrazie7@uncc.edu>
2024-07-14 17:25:36 -0400

    add liked icon buttton to cards

--- src/components/pages/Dashboard.tsx
+++ src/components/pages/Dashboard.tsx
@@ -9,11 +9,14 @@ import {
   SelectChangeEvent,
   Typography,
 } from "@mui/material";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import styles from "../../styling/dashboard.module.css";
 
 function Dashboard(props: any) {
   const location = useLocation();
   const selected = location.state.category;
+  const [liked, setLiked] = React.useState(false);
   //   const selected = location.state.category;
   const engineeringItems = womenInTechnology.filter(
     (item) => item.category === "Engineering"
@@ -33,18 +36,39 @@ function Dashboard(props: any) {
   const entrepeneurshipItems = womenInTechnology.filter(
     (item) => item.category === "Entrepeneurship"
   );
+  const handleChange = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setLiked(true);
+  };
   console.log(selected);
   return (
     <>
       {selected === "Engineering" && (
         <div className={styles["responsive-two-column-grid"]}>
           {engineeringItems.map((item) => (
-            <Card sx={{width:"300px", height:"350px"}}>
+            <Card sx={{ width: "300px", height: "350px" }}>
               <CardContent>
-              <Typography variant="body2" color="text.seconday">
-                <p id={item.id}>{item.name}</p>
-                <img src={item.image_path} key={item.name} alt="engineering image"/>
-              </Typography>
+                <Typography variant="body2" color="text.seconday">
+                  <p id={item.id}>
+                    {item.name}
+
+                    <IconButton onClick={handleChange} type="button">
+                      {liked ? (
+                        <FavoriteBorderOutlinedIcon />
+                      ) : (
+                        <FavoriteIcon />
+                      )}
+                    </IconButton>
+                  </p>
+                  <img
+                    src={item.image_path}
+                    key={item.name}
+                    alt="engineering image"
+                  />
+                  <p>
+                    Learn more about {item.name} <a href={item.website}>here</a>
+                  </p>
+                </Typography>
               </CardContent>
             </Card>
           ))}
