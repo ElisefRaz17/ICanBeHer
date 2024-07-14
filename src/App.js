ed939cc6c1ab6753fece6198369c4f031c16748a
Elise Frazier <efrazie7@uncc.edu>
2024-07-14 13:37:56 -0400

    fixing state management

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {useState, createContext, useContext} from "react";
 import "./App.css";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./components/Navbar";
@@ -7,24 +8,29 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import Home from "./components/pages/Home";
+import Hero from "./components/Hero";
 import AspirationOutput from "./components/pages/AspirationOutput";
 import Signup from "./components/pages/Signup";
 import Login from "./components/pages/Login";
+import Dashboard from "./components/pages/Dashboard";
 
-
+export const StateContext = createContext();
 function App() {
+  const [aspiration, setAspiration] = React.useState("");
   return (
+    <StateContext.Provider value={{aspiration,setAspiration}}>
     <Router>
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/" element={<Hero/>}/>
         <Route path="/aspiration" element={<AspirationOutput/>}/>
         <Route path="/register" element={<Signup/>}/>
         <Route path="/sign-in" element={<Login/>}/>
+        <Route path="/dashboard" element={<Dashboard/>}/>
       </Routes>
       <Footer/>
     </Router>
+    </StateContext.Provider>
   );
 }
 
--- src/components/pages/Dashboard.tsx
+++ src/components/pages/Dashboard.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import womenInTechnology from "../data";
+import { useLocation } from "react-router-dom";
+import {
+  Button,
+  Card,
+  CardContent,
+  IconButton,
+  SelectChangeEvent,
+  Typography,
+} from "@mui/material";
+import styles from "../../styling/footer.module.css";
 
 function Dashboard() {
-  return (
-    <div>Dashboard</div>
-  )
+  const location = useLocation();
+  const selected = location.state.selected;
+  const engineeringItems = womenInTechnology.filter(
+    (item) => item.category === "Engineering"
+  );
+  const dataScienceItems = womenInTechnology.filter(
+    (item) => item.category === "Data Science"
+  );
+  const softwareEngineeringItems = womenInTechnology.filter(
+    (item) => item.category === "Software Engineering"
+  );
+  const designItems = womenInTechnology.filter(
+    (item) => item.category === "Design"
+  );
+  const cyberSecurityItems = womenInTechnology.filter(
+    (item) => item.category === "Cybersecurity"
+  );
+  const entrepeneurshipItems = womenInTechnology.filter(
+    (item) => item.category === "Entrepeneurship"
+  );
+   console.log(selected);
+  return <>{selected === "engineering" && <Card></Card>}</>;
 }
 
-export default Dashboard
+export default Dashboard;
